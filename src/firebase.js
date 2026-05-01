b27e32d169ef49b4165a476f74f4c729b47f8d07
abbasdemir000-beep <abbasdemir000@gmail.com>
2026-05-01 08:19:57 +0000

    Firebase connected

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnnXeS8AaCdx7Zlg7c8Z9bz_YtqJ0H9Js",
+  authDomain: "gen-lang-client-0030552055.firebaseapp.com",
+  projectId: "gen-lang-client-0030552055",
+  storageBucket: "gen-lang-client-0030552055.firebasestorage.app",
+  messagingSenderId: "383437920795",
+  appId: "1:383437920795:web:59d975aed9b0f2e40276be"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-
+  import "./firebase";
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
   import "./styles/index.css";
