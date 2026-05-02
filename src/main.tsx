5d943c714f9caa6287f53cb70915a567735fa375
abbasdemir000-beep <abbasdemir000@gmail.com>
2026-05-02 20:22:09 +0300

    Remove old Firebase import from main

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import "./firebase";
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
@@ -6,4 +5,3 @@ import "./styles/index.css";
 createRoot(document.getElementById("root")!).render(
   <App />
 );
-  
